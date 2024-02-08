c749360099624eba4e03145f45bb2a4a88b51082
Oliver baltzer <114311552+ObaltzerS@users.noreply.github.com>
2024-02-07 22:56:06 -0800

    restructured to use interface.py

--- interface.py
+++ interface.py
@@ -8,23 +8,45 @@ class Interface():
 
     methods:
     init_args : initializes the list of arguements that interface can recognize
-
+    init_parser : initializes the arguement parser
     """
     def __init__(self):
-        self.parser = argparse.ArgumentParser()
-        self.init_args()
+        self.init_parser()
         self.parser.parse_args(namespace = self)
 
+    def init_parser(self):
+        """
+        initialize the arguement parser
+        """
+        self.parser = argparse.ArgumentParser()
+        self.init_args()
+
     def init_args(self):
-        self.parser.add_argument("-p", metavar="play",action = "extend",nargs = "+" ,help = "play sound file")
-        self.parser.add_argument("-l", metavar="list", action = "store",help = "list sounds in directory")
-        self.parser.add_argument("-rn", metavar="rename", action = "store", help = "rename sound")
+        """
+        initializes list of commands for CLI
+        """
+        self.parser.add_argument("-p","--play",metavar="play",action = "extend", nargs = "+" ,help = "play sound ")
+        self.parser.add_argument("-l","--list",metavar="list",action = "store",help = "list sounds in directory")
+        self.parser.add_argument("-rn","--rename",metavar="rename",action = "store", help = "rename sound")
 
     def play_file(self):
+        """
+        placeholder
+        """
         pass
 
     def list_files(self):
+        """
+        placeholder
+        """
         pass
 
     def rename_file(self):
+        """
+        placeholder
+        """
         pass 
+
+interface = Interface()
+
+interface
--- simpleaudio_test.py
+++ simpleaudio_test.py
@@ -1,52 +1,15 @@
 import sys
 import simpleaudio as sa
+from interface import Interface
 
 def play_sound(filename):
     wave_obj = sa.WaveObject.from_wave_file(filename)
     play_obj = wave_obj.play()
     play_obj.wait_done()  # Wait until sound has finished playing
 
-'''
-# HELP: This is a command line argument that is invoked by typing --help or -h
-(Note that it is a command line convention to offer full name and first letter options.
-The full name options have two preceding dashes -- and the one-letter options have just one -)
-For example:
->> python cli_example.py --help
->> python cli_example.py --h
-Right now, this help command is not very useful.
-Take a look at some other helps commands, like
->> python --help
->> pip -h
->> conda --help
-To see what they include.
-'''
+interface = Interface()
 
-# This if statement checks to see what argument comes right after the program name
-# Note that it also runs if you have no arguments after the program name
-if len(sys.argv)<=1 or sys.argv[1]=='--help' or sys.argv[1]=='-h':
-
-    # This prints out a sample of how you might use this command
-    print("usage:",sys.argv[0], '--help')
-
-    help_message = """
-    Usage: python cli_example.py [command] [arguments]
-    Commands:
-    -c, --count    : Count the number of arguments.
-    -p, --play     : Play the specified sound file. Usage: -p <filepath>
-    -h, --help     : Show this help message.
-    """
-    print(help_message)
-    # Hygiene
-    sys.exit(0)
+for filename in interface.play:
+    play_sound(filename)
 
 
-if sys.argv[1] == '-p' or sys.argv[1] == '--play' :
-    for filename in sys.argv[2:]:
-        play_sound(filename)
-    sys.exit(0)
-
-
-"""
-possible restructure to accept commands after running the program rather than just on initial 
-command line arguements
-"""
