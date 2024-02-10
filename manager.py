705831a54d719dea47866f76dc394f22e204cd2e
Oliver baltzer <114311552+ObaltzerS@users.noreply.github.com>
2024-02-10 13:11:25 -0800

    rename file implement

--- manager.py
+++ manager.py
@@ -1,45 +1,84 @@
 import sys
+import os
 from audio import *
 
-class Controller:
-    """
-    
-    """
-    def __init__(self,files):
-        player=Player()
-        for file in files:
-            player.playsound(file)
-        print("Controller")
-
-
-class FileManager:
-    """
-    
-    """
+class FileManager():
     def __init__(self):
-        print("File Manager")
+        self.os = os
+        self.directories = self.os.listdir("./sounds/")
+
+    def rename(self, dir, oldFile, newFile):
+
+        """
+        This method will take in a target directory, target file, and a new file name for the
+        target file the target file will be renamed to the new file name and kept in the same
+        directory.
+
+        TODO: 
+
+        """
+
+        if dir in self.directories:
+            path = "./sounds/" + str(dir) + "/"  
+            try:
+                self.os.rename(path + str(oldFile), path + str(newFile) )
+            except FileNotFoundError:
+                print(path + str(oldFile))
+                print("file: " + str(oldFile) + " not found")
+            except FileExistsError: 
+                print("file: " + str(newFile) + " already exists")
+        else:
+            print("directory not found")
+
+
+
+    def delete(self, fileName):
+        """
+        This method will delete a specific file from a directory given the path to that file.
+
+
+        TODO: 
+         - tests
+         - should we take directory as an arguemnt or the relative path to that file?
+         - 
+        """
+        try:
+            self.os.remove(fileName)
+        except FileNotFoundError:
+            print("file" + str(fileName) + "not found")
+
+    def add_file(self, filePath, dest):
+        pass
+
+    def list_files(self, dest):
+        pass
         
 def main():
-    if len(sys.argv)<=1 or sys.argv[1]=='--help' or sys.argv[1]=='-h':
-        # This prints out a sample of how you might use this command
-        print("usage:",sys.argv[0], '--help')
 
-        help_message = """
-        Usage: python cli_example.py [command] [arguments]
-        Commands:
-        -c, --count    : Count the number of arguments.
-        -p, --play     : Play the specified sound file. Usage: -p <filepath>
-        -h, --help     : Show this help message.
-        """
-        print(help_message)
-        # Hygiene
-        sys.exit(0)
-    if sys.argv[1] == '-p' or sys.argv[1] == '--play' :
-        Control=Controller(sys.argv[2:])
-        sys.exit(0)
+    filemanager = FileManager()
+
+    filemanager.rename("old-sounds", "toaster.wav", "toaster.wav")
+
+    # if len(sys.argv)<=1 or sys.argv[1]=='--help' or sys.argv[1]=='-h':
+    #     # This prints out a sample of how you might use this command
+    #     print("usage:",sys.argv[0], '--help')
+
+    #     help_message = """
+    #     Usage: python cli_example.py [command] [arguments]
+    #     Commands:
+    #     -c, --count    : Count the number of arguments.
+    #     -p, --play     : Play the specified sound file. Usage: -p <filepath>
+    #     -h, --help     : Show this help message.
+    #     """
+    #     print(help_message)
+    #     # Hygiene
+    #     sys.exit(0)
+    # if sys.argv[1] == '-p' or sys.argv[1] == '--play' :
+    #     Control=Controller(sys.argv[2:])
+    #     sys.exit(0)
     
-    if sys.argv[1]=='-c' or sys.argv[1]=='--count':
-        return len(sys.argv)-2
+    # if sys.argv[1]=='-c' or sys.argv[1]=='--count':
+    #     return len(sys.argv)-2
             
             
     
