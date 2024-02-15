a4b7603fde7e65d9282716c6f6f22ad00eb500a2
gabepm100 <gabepm100@gmail.com>
2024-02-14 18:29:49 -0800

    layering working

--- audio.py
+++ audio.py
@@ -1,5 +1,7 @@
 import sys
 import simpleaudio as sa
+import os
+from os import walk
 
 class Player:
     """
@@ -11,21 +13,31 @@ class Player:
     def play(self,filename):
         wave_obj = sa.WaveObject.from_wave_file(filename)
         play_obj = wave_obj.play()
-        play_obj.wait_done()  # Wait until sound has finished playing
+        play_obj.wait_done()  # Wait until sound has finished playing    
         
     def isPlaying(self):
         print("is playing")
         
-        
-class AudioEffects:
+class AudioEffects(Player):
     """
     
     """
     def __init__(self):
         print("AudioEffects")
         
-    def layer(self):
-        print("layer")
+    def layer(self,files):
+        wavlist=[]
+        for file in files:
+            if file.endswith('.wav'):
+                wavlist.append(sa.WaveObject.from_wave_file(file))
+        playlist=[]
+        for wave in wavlist:
+            playlist.append(wave.play())
+        for wave in playlist:
+            wave.wait_done()  # Wait until sound has finished playing
+            
+            
+            #self.play(file)
         
     def backward(self):
         print("backward")
--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
 import argparse
+from audio import * 
 
 class Interface():
     """
@@ -15,6 +16,10 @@ class Interface():
 
         self.init_parser()
         self.parser.parse_args(namespace = self)
+        AE=AudioEffects()
+        AE.layer(self.get_layer_args())
+        
+
 
         #self.play # these attributes are created on call of parse_args
         #self.list
@@ -43,7 +48,9 @@ class Interface():
         self.parser.add_argument("-p","--play",metavar="play",action = "extend", nargs = "+" ,help = "play sound ")
         self.parser.add_argument("-l","--list",metavar="list",action = "store",help = "list sounds in directory")
         self.parser.add_argument("-rn","--rename",metavar="rename",action = "store", help = "rename sound")
-
+        self.parser.add_argument("-ly","--layer", metavar="layer", action="extend",nargs="+",help="layer the audio")
+        self.parser.add_argument("-sq","--sequence",metavar="sequence", action="extend",nargs="+",help="sequences one sound after another")
+#-l 
     def get_play_args(self):
         """
         return the arguemnts passed in with the -p command
@@ -62,6 +69,40 @@ class Interface():
         returns the arguements passed in with the -rn command
         """
         return self.rename
+    
+    def get_layer_args(self):
+
+        """
+        returns the arguements passed in with the -ly command
+        """
+        l=[]
+        if self.layer==None:
+            return None
+        for item in self.layer:
+            if os.path.isfile(item):
+                l.append(item)
+            elif os.path.isdir(self.layer[0]):
+                for (dir_path,dir,files) in walk(item):
+                    for file in files:
+                        l.append('./'+dir_path+'/'+file)
+        return l
+    
+    def get_sequence_args(self):
+
+        """
+        returns the arguements passed in with the -ly command
+        """
+        
+        return self.sequence
+    
+    def delegate(self,type):
+        """
+        should delegate the arguments that were recieved and parsed to their individual class so that they can run them
+
+        Args:
+            type:
+        """
+        return type
 
 
 interface = Interface()
