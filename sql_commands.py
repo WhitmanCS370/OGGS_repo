d6cf778ecffde1c08ab970611edbf7d7d39d0236
Steven <steven.lin@leadvisor.net>
2024-04-02 15:13:31 -0700

    organized sql commands, changed database_init to wipe all data before init

--- sql_commands.py
+++ sql_commands.py
@@ -31,6 +31,25 @@ class databaseManager():
     #         VALUES (?, ?, ?, ?, ?, ?)
     #     """, (title, artist, album, genre, insertPath, duration))
     #     self.conn.commit()
+        
+    # START SONG METHODS
+    def get_song_id(self, filename):
+        self.cursor.execute(
+            """
+                SELECT DISTINCT id FROM audio_files
+                WHERE title == (?);
+            """,(filename,)
+        )
+        songid = self.cursor.fetchall()
+        return songid[0][0]
+    def get_duration(self, filepath): 
+        with wave.open(filepath, 'rb') as wf:
+            duration = float(wf.getnframes()) / wf.getframerate()
+        return duration
+    
+    # END SONG METHODS
+    
+    # START PLAYLIST METHODS
 
     def get_playlist_id(self, playlist):
         self.cursor.execute(
@@ -41,42 +60,17 @@ class databaseManager():
         )
         playlistid = self.cursor.fetchall()
         return playlistid[0][0]
+    
+    def list_playlists(self):
+        """
+        list all playlists
+        """
+        self.cursor.execute("""
+            SELECT DISTINCT name FROM playlists;
+        """)
+        playlists = np.array(self.cursor.fetchall())
+        return playlists.ravel()
 
-    def get_song_id(self, filename):
-        self.cursor.execute(
-            """
-                SELECT DISTINCT id FROM audio_files
-                WHERE title == (?);
-            """,(filename,)
-        )
-        songid = self.cursor.fetchall()
-        return songid[0][0]
-    
-    def get_tag_id(self, tag):
-        self.cursor.execute(
-            """
-                SELECT DISTINCT id FROM tags
-                WHERE name == (?);
-            """,(tag,)
-        )
-        tagid = self.cursor.fetchall()
-        return tagid[0][0]
-
-    def get_filepath(self, filename):
-        self.cursor.execute(
-            """
-                SELECT DISTINCT title, filepath FROM audio_files
-                WHERE title == (?);
-            """,(filename,)
-        )
-        path = self.cursor.fetchall()
-        return path
-    
-    def get_duration(self, filepath): 
-        with wave.open(filepath, 'rb') as wf:
-            duration = float(wf.getnframes()) / wf.getframerate()
-        return duration
-    
     def get_playlist(self, playlist):
         """
         get all songs in a playlist
@@ -90,23 +84,7 @@ class databaseManager():
         """, (playlistid,))
         files = self.cursor.fetchall()
         return files
-
-    def add_from_file(self, title, filepath, artist = None, album = None, genre = None):
-        """
-        album, artist, genre allowed null
-        calculate duration from filepath
-        """
-        duration = self.get_duration(filepath)
-        try:
-            self.cursor.execute("""
-                INSERT INTO audio_files (title, artist, album, genre, filepath, duration)
-                VALUES (?, ?, ?, ?, ?, ?);
-            """, (title, artist, album, genre, filepath, duration))
-            self.conn.commit()
-        except sqlite3.IntegrityError:
-            print("File already exists in the database.")
-            return
-
+    
     def add_playlist(self, name):
         """
         This method will add a new playlist to the database.
@@ -128,17 +106,36 @@ class databaseManager():
             VALUES (?, ?)
         """, (playlistid, songid))
         self.conn.commit()
-
     
-    def list_playlists(self):
+    # END PLAYLIST METHODS
+
+    # START FILE METHODS
+        
+    def get_filepath(self, filename):
+        self.cursor.execute(
+            """
+                SELECT DISTINCT title, filepath FROM audio_files
+                WHERE title == (?);
+            """,(filename,)
+        )
+        path = self.cursor.fetchall()
+        return path
+
+    def add_from_file(self, title, filepath, artist = None, album = None, genre = None):
         """
-        list all playlists
+        album, artist, genre allowed null
+        calculate duration from filepath
         """
-        self.cursor.execute("""
-            SELECT DISTINCT name FROM playlists;
-        """)
-        playlists = np.array(self.cursor.fetchall())
-        return playlists.ravel()
+        duration = self.get_duration(filepath)
+        try:
+            self.cursor.execute("""
+                INSERT INTO audio_files (title, artist, album, genre, filepath, duration)
+                VALUES (?, ?, ?, ?, ?, ?);
+            """, (title, artist, album, genre, filepath, duration))
+            self.conn.commit()
+        except sqlite3.IntegrityError:
+            print("File already exists in the database.")
+            return
 
     def list_files(self):
         """
@@ -150,6 +147,41 @@ class databaseManager():
         files = np.array(self.cursor.fetchall())
         return files.ravel()
 
+    def rename(self, oldFileName, newFileName):
+        """
+        rename a file in the database
+        Does not rename the file in the file system
+        """
+        self.cursor.execute("""
+            UPDATE audio_files
+            SET title = (?)
+            WHERE title = (?);
+        """, (newFileName, oldFileName))
+        self.conn.commit()
+    
+    # END FILE METHODS
+        
+    # START TAG METHODS
+    
+    def get_tag_id(self, tag):
+        self.cursor.execute(
+            """
+                SELECT DISTINCT id FROM tags
+                WHERE name == (?);
+            """,(tag,)
+        )
+        tagid = self.cursor.fetchall()
+        return tagid[0][0]
+
+    def list_tags(self):
+        """
+        show all tags
+        """
+        self.cursor.execute("""
+            SELECT DISTINCT name FROM tags;
+        """)
+        tags = np.array(self.cursor.fetchall())
+        return tags.ravel()
 
     def add_tag(self, name, desc):
         """
@@ -160,7 +192,6 @@ class databaseManager():
             VALUES (?, ?)
         """, (name, desc))
         self.conn.commit()
-
 
     def add_tag_to_file(self, tag, filename):
         """
@@ -189,26 +220,11 @@ class databaseManager():
         res = []
         return files.ravel()
     
-    def rename(self, oldFileName, newFileName):
-        """
-        rename a file in the database
-        """
-        self.cursor.execute("""
-            UPDATE audio_files
-            SET title = (?)
-            WHERE title = (?);
-        """, (newFileName, oldFileName))
-        self.conn.commit()
+    # END TAG METHODS
 
-    def list_tags(self):
-        """
-        show all tags
-        """
-        self.cursor.execute("""
-            SELECT DISTINCT name FROM tags;
-        """)
-        tags = np.array(self.cursor.fetchall())
-        return tags.ravel()
+    
+    
+    
 
 # helper methods for testing
     def clear_tables(self):
