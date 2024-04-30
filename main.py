ddcf614a553807b87cbb9cadb1dcae32f18a269c
Oliver baltzer <114311552+ObaltzerS@users.noreply.github.com>
2024-04-30 15:46:04 -0700

    fix main

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from gui import gui
+from gui import open_gui
 from interface import Interface
 from sys import argv
 
 if (__name__ == "__main__"):
     if "-g" in argv:
-        i = gui()
+        open_gui()
     else:
         i = Interface()
         i.cmdloop()
